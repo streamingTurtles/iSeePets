6574860a772be455f99b0b7ada67a61035fc446e
streamingturtles.com <pcardillo@streamingturtles.com>
2020-12-29 01:56:44 -0500

    deleted an old branch (pac12/22/2020 was 20a6600) that was preventing updating to my local branch since it wasin a rebase that I was unable to merge - something about a missing head??? - I also manually deleted in gitHub on the web

---
+++ frontend.js
@@ -0,0 +1,36 @@
+defaultIsDog()
+function defaultIsDog(){
+  document.getElementById("dogs").style.display = "block";
+  
+}
+function changetabs(evt, tabname) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabname).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+
+
+var apiKey= "i4WSkGk27C4yI7wCconCIHNXNrZh2dLsepStJSgNB7p9137P05";
+var url = "https://api.petfinder.com/v2/animals";
+
+$.get({
+//   url: "https://api.petfinder.com/v2/animals",
+  url: "https://cors-anywhere.herokuapp.com/",
+  method: "Get",
+  headers: {
+      Authorization:" ",
+  }
+}).then((res) => {
+  console.log(res);
+});
+
+
