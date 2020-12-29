4b8fabac9f4b6b0279ca612e014aea17ac626544
streamingturtles.com <pcardillo@streamingturtles.com>
2020-12-29 08:28:58 -0500

    many changes: added googel Map to index page, repositioned scripts in index.html file, removed iFram (not needed currently), deleted my testing branch pac12/22/2020 (was 20a6600), implemented fetch JS browser API as alternative to , successful json object returns after API call is made, updated token request function so a new token is generated for each API query (note: the token expires after 3600sec = 1hr), cleaned up syntax, reArrainged fronEnd form input - to discuss with team on this at next scrum

--- backend.js
+++ backend.js
@@ -1,33 +1,101 @@
+// ****************************************************************************** //
+// from petfinder.com/user/developer-settings  -  API key & Secrete
+var key="i4WSkGk27C4yI7wCconCIHNXNrZh2dLsepStJSgNB7p9137P05";
+var secret="Dav2xXd8H9Y7PTvQvBymZPdYAP3KYXYTLWMu5Eca";
 
-      //.post to get generated api token
-      function getapiKey() {
-        $.post(
-          "https://api.petfinder.com/v2/oauth2/token",
-          "grant_type=client_credentials&client_id=i4WSkGk27C4yI7wCconCIHNXNrZh2dLsepStJSgNB7p9137P05&client_secret=Dav2xXd8H9Y7PTvQvBymZPdYAP3KYXYTLWMu5Eca",
-          function (response) {
-            // console.log(response);
-            // console.log(response.access_token);
-            return response.access_token;
-          }
-        );
+
+// from https://www.petfinder.com/developers/v2/docs/ 
+// from documentation : use deconstructed curl to browser fetch to get a new token for each API request 
+// curl -d "grant_type=client_credentials&client_id={CLIENT-ID}&client_secret={CLIENT-SECRET}" https://api.petfinder.com/v2/oauth2/token  
+
+
+var token=""
+
+function getNewToken() {
+    console.log("getting a NEW TOKEN, they expire every 3600 seconds: ", token);
+    fetch('https://api.petfinder.com/v2/oauth2/token', {
+        method: "POST",
+        // https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/POST
+        body: "grant_type=client_credentials&client_id=" + key + "&client_secret=" + secret,
+         headers: {
+           "Content-Type": "application/x-www-form-urlencoded"
+          //  https://dev.to/sidthesloth92/understanding-html-form-encoding-url-encoded-and-multipart-forms-3lpa
+        }
+    }).then(function(resp) {
+    return resp.json();
+    }).then(function(data) {
+    token= data
+    console.log('new token updated: ', data)})
+}
+getNewToken();
+// ****************************************************************************** //
+
+
+
+
+
+
+
+
+
+// ****************************************************************************** //
+// retrieves access token & makes API call to get animals
+function apiCallForAnimals(animalType, zip, breed) {
+  console.log("GET ME SOME ANIMALS - API CALL MADE");
+  fetch(
+    `https://api.petfinder.com/v2/animals?type=${animalType}&breed=${breed}&location=${zip}&status=adoptable&distance=25&limit=100`,
+    {
+      headers: {
+        // FROM THE DOCUMENTATION
+        // https://www.petfinder.com/developers/v2/docs/#get-animal-types
+        // token comes back in object form as: 
+        // {
+        //   "token_type": "Bearer",
+        //   "expires_in": 3600,
+        //   "access_token": "..."
+        // }
+        Authorization: `Bearer ${token.access_token}`
       }
-
-      var apiKey = getapiKey();
-      var url = "https://api.petfinder.com/v2/animals";
-
-      $.get({
-        url:
-          "https://cors-anywhere.herokuapp.com/https://api.petfinder.com/v2/animals",
-        method: "GET",
-        headers: {
-          Authorization:
-            "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJpNFdTa0drMjdDNHlJN3dDY29uQ0lITlhOclpoMmRMc2VwU3RKU2dOQjdwOTEzN1AwNSIsImp0aSI6IjIyZDViMTQ3M2JjYTdkNzk0NDQwNGE2ODVlMDY1MGEwMTdhMzk2YjRiNGZlNDRhNWExNTM5ODE4ODEwNmJjOTEwYWM5YmNlOTdhYmJkZjFmIiwiaWF0IjoxNjA4NDAyMzgyLCJuYmYiOjE2MDg0MDIzODIsImV4cCI6MTYwODQwNTk4Miwic3ViIjoiIiwic2NvcGVzIjpbXX0.idXHWoYk_-Bqi1f7TyTfQtIYFurFzxqn_On_UY9bA28oSQ9vyeY-9CayTddGIfMATxLHDoC4lZGlFCN5Rqhcz0ADQWPW3XuVi7aBRXQ1zoR78tbp9hWPIrfSseqf02LLiZW9Y5pk1R-0Kz6YXPe36T6vFbf6KKPw6Thamb9exYb9P8VUy3JaFgqJhlhKmDRAz6VOo8ilXmMVb7ymh1DcuSpWA_nvlMaDSi_s_jYS-Lw9PhUDP2_I05u8H411Axai6iNAxhshCxJcNoM1j8VBLEDe5C7cCeCzfADGk5-AIsmMfsAIboMNoDFZItkUotnTITh6d35YgAqdk30jtjYwpg",
-        },
-      }).then((res) => {
-        console.log(res);
-        $(".breed").text(" Breed:" + res.animals[0].breeds.primary);
-      });
-
-    function searchPets(){
-      window.location.href = "selection.html"
     }
+  )
+  // .then(function(response) {
+  //   var json = response.json();
+  //   console.log("here is an ANIMAL OBJECT: ", json);
+  // }); 
+  .then(response => response.json())
+  .then(responseJson => getMeSomeAnimals(responseJson))
+
+}    
+
+
+
+function getMeSomeAnimals(responseJson) {
+  console.log(responseJson);
+}
+// ****************************************************************************** //
+
+
+
+
+
+
+
+
+
+
+function searchPets() {
+  console.log("API search Request Made - NOW go get me some ANIMALS !!!");
+  $("form").submit(event => {
+    alert("Submitted"); // testing
+    event.preventDefault();  // do not submit to server, input used for building API call
+    console.log("******** FORM SUBMITTED - HERE ARE YOUR SELETIONS TO BUILD API QUERY *********")
+    // var animalType = $(".tablinks").val(); 
+    var animalType = $("#petType").val(); 
+    var zip = $("#zip").val();
+    var breed = $("#dogbreed").val();
+    console.log("you are looking for a: ", animalType);
+    console.log("the breed you selected is: ", breed);
+    console.log("your zip code is: ", zip);
+    apiCallForAnimals(animalType, zip, breed);
+  });  
+}
